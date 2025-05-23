8df3cf6bd827d661da6627411a731d742244fab8
Vishruth V Srivatsa <vishruthvs10@gmail.com>
2025-05-23 17:41:47 +0530

    [ Backend ] : Implement JWT Auth

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import Home
+
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('', Home.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-def home(request):
-    return HttpResponse("Hello World")
+class Home(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
